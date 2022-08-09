094c87fc3bdd6416348cabac72613eccdf2a87fb
tiroleyi <tiroleyi@tencent.com>
2022-08-09 15:22:38 +0800

    104

---
+++ 104-maximumDepthOfBinaryTree.go
@@ -0,0 +1,53 @@
+//Given the root of a binary tree, return its maximum depth.
+//
+// A binary tree's maximum depth is the number of nodes along the longest path
+//from the root node down to the farthest leaf node.
+//
+//
+// Example 1:
+//
+//
+//Input: root = [3,9,20,null,null,15,7]
+//Output: 3
+//
+//
+// Example 2:
+//
+//
+//Input: root = [1,null,2]
+//Output: 2
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in the tree is in the range [0, 10⁴].
+// -100 <= Node.val <= 100
+//
+//
+// Related Topics Tree Depth-First Search Breadth-First Search Binary Tree 👍 79
+//57 👎 137
+
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	return max(left, right) + 1
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
