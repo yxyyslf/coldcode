a704029bfac738653fc67d6eb5aefd4c4ebf25dc
tiroleyi <tiroleyi@tencent.com>
2022-08-09 10:19:07 +0800

    142

---
+++ 142-linkedListCycleII.go
@@ -0,0 +1,89 @@
+//Given the head of a linked list, return the node where the cycle begins. If
+//there is no cycle, return null.
+//
+// There is a cycle in a linked list if there is some node in the list that can
+//be reached again by continuously following the next pointer. Internally, pos is
+//used to denote the index of the node that tail's next pointer is connected to (0
+//-indexed). It is -1 if there is no cycle. Note that pos is not passed as a
+//parameter.
+//
+// Do not modify the linked list.
+//
+//
+// Example 1:
+//
+//
+//Input: head = [3,2,0,-4], pos = 1
+//Output: tail connects to node index 1
+//Explanation: There is a cycle in the linked list, where tail connects to the
+//second node.
+//
+//
+// Example 2:
+//
+//
+//Input: head = [1,2], pos = 0
+//Output: tail connects to node index 0
+//Explanation: There is a cycle in the linked list, where tail connects to the
+//first node.
+//
+//
+// Example 3:
+//
+//
+//Input: head = [1], pos = -1
+//Output: no cycle
+//Explanation: There is no cycle in the linked list.
+//
+//
+//
+// Constraints:
+//
+//
+// The number of the nodes in the list is in the range [0, 10⁴].
+// -10⁵ <= Node.val <= 10⁵
+// pos is -1 or a valid index in the linked-list.
+//
+//
+//
+// Follow up: Can you solve it using O(1) (i.e. constant) memory?
+//
+// Related Topics Hash Table Linked List Two Pointers 👍 8353 👎 574
+
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	return slow
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
