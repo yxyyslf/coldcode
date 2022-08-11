0053f250d931f55281d2dccb37c307983f92ce99
tiroleyi <tiroleyi@tencent.com>
2022-08-11 14:41:39 +0800

    62

---
+++ 62-uniquePaths.go
@@ -0,0 +1,62 @@
+//There is a robot on an m x n grid. The robot is initially located at the top-
+//left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right
+//corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at
+//any point in time.
+//
+// Given the two integers m and n, return the number of possible unique paths
+//that the robot can take to reach the bottom-right corner.
+//
+// The test cases are generated so that the answer will be less than or equal
+//to 2 * 10⁹.
+//
+//
+// Example 1:
+//
+//
+//Input: m = 3, n = 7
+//Output: 28
+//
+//
+// Example 2:
+//
+//
+//Input: m = 3, n = 2
+//Output: 3
+//Explanation: From the top-left corner, there are a total of 3 ways to reach
+//the bottom-right corner:
+//1. Right -> Down -> Down
+//2. Down -> Down -> Right
+//3. Down -> Right -> Down
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= m, n <= 100
+//
+//
+// Related Topics Math Dynamic Programming Combinatorics 👍 11178 👎 334
+
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func uniquePaths(m int, n int) int {
+	pathDp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		pathDp[i] = make([]int, n)
+		pathDp[i][0] = 1
+	}
+	for i := 0; i < n; i++ {
+		pathDp[0][i] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			pathDp[i][j] = pathDp[i-1][j] + pathDp[i][j-1]
+		}
+	}
+
+	return pathDp[m-1][n-1]
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
