32e3a473212e1d5c6e49d7a9ce69a6bdc1f1f749
tiroleyi <tiroleyi@tencent.com>
2022-08-15 17:45:27 +0800

    19

--- 19-removeNthFromEnd.go
+++ 19-removeNthFromEnd.go
@@ -1,24 +1,24 @@
 package main
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-		if head == nil {
-			return nil
-		}
-
-		slow := head
-		fast := head
-		for i:=0; i < n;i++ {
-			fast = fast.Next
-		}
+	if head == nil {
+		return nil
+	}
+	dummy := &ListNode{
+		Next: head,
+	}
+	slow := dummy
+	fast := dummy
+	for i := 0; i < n; i++ {
+		fast = fast.Next
 		if fast == nil {
-			return head.Next
+			return head
 		}
-
-
-		for fast != nil {
-			slow = slow.Next
-			fast = fast.Next
-		}
-		slow.Next = slow.Next.Next
-		return head
+	}
+	for fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
 }
