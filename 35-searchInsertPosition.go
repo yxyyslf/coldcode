055235613b45bb573f7700809a442878661e23f1
tiroleyi <tiroleyi@tencent.com>
2022-08-15 10:42:52 +0800

    35

---
+++ 35-searchInsertPosition.go
@@ -0,0 +1,60 @@
+//Given a sorted array of distinct integers and a target value, return the
+//index if the target is found. If not, return the index where it would be if it were
+//inserted in order.
+//
+// You must write an algorithm with O(log n) runtime complexity.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [1,3,5,6], target = 5
+//Output: 2
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [1,3,5,6], target = 2
+//Output: 1
+//
+//
+// Example 3:
+//
+//
+//Input: nums = [1,3,5,6], target = 7
+//Output: 4
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= nums.length <= 10⁴
+// -10⁴ <= nums[i] <= 10⁴
+// nums contains distinct values sorted in ascending order.
+// -10⁴ <= target <= 10⁴
+//
+//
+// Related Topics Array Binary Search 👍 9057 👎 449
+
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func searchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
