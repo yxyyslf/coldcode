e9426a56dcfd35397f9b7b69c70d24001adc2438
tiroleyi <tiroleyi@tencent.com>
2022-08-19 14:29:58 +0800

    31

--- 31-nextPermutation.go
+++ 31-nextPermutation.go
@@ -1,40 +1,98 @@
+//A permutation of an array of integers is an arrangement of its members into a
+//sequence or linear order.
+//
+//
+// For example, for arr = [1,2,3], the following are considered permutations of
+//arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
+//
+//
+// The next permutation of an array of integers is the next lexicographically
+//greater permutation of its integer. More formally, if all the permutations of the
+//array are sorted in one container according to their lexicographical order,
+//then the next permutation of that array is the permutation that follows it in the
+//sorted container. If such arrangement is not possible, the array must be
+//rearranged as the lowest possible order (i.e., sorted in ascending order).
+//
+//
+// For example, the next permutation of arr = [1,2,3] is [1,3,2].
+// Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+// While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does
+//not have a lexicographical larger rearrangement.
+//
+//
+// Given an array of integers nums, find the next permutation of nums.
+//
+// The replacement must be in place and use only constant extra memory.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [1,2,3]
+//Output: [1,3,2]
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [3,2,1]
+//Output: [1,2,3]
+//
+//
+// Example 3:
+//
+//
+//Input: nums = [1,1,5]
+//Output: [1,5,1]
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 100
+//
+//
+// Related Topics Array Two Pointers 👍 12362 👎 3692
+
 package main
 
-import "math"
+//leetcode submit region begin(Prohibit modification and deletion)
+func nextPermutation(nums []int) {
+	if nums == nil || len(nums) == 0 {
+		return
+	}
+	n := len(nums)
+	i, j, k := n-2, n-1, n-1
 
-func nextPermutation(nums []int)  {
-	index := 0
-	reverseFlag := 0
-	for i:=len(nums)-1;i >= 1;i-- {
-		if nums[i-1] < nums[i] {
-			index = i-1
-			reverseFlag = 1
+	for i >= 0 && nums[i] >= nums[j] {
+		i--
+		j--
+	}
+
+	if i < 0 {
+		permutationReverse(nums, 0, n-1)
+		return
+	}
+
+	for ; k >= j; k-- {
+		if nums[k] > nums[i] {
+			nums[k], nums[i] = nums[i], nums[k]
 			break
 		}
 	}
-	if reverseFlag==0 {
-		permutationReverse(nums)
-		return
-	}
-	changeIndex := index+1
-	minVal := math.MaxInt16
-	for i:=len(nums)-1;i > index;i-- {
-		if nums[i] > nums[index] && nums[i] - nums[index] < minVal{
-			minVal = nums[i] - nums[index]
-			changeIndex = i
-		}
-	}
-
-	nums[index], nums[changeIndex] = nums[changeIndex], nums[index]
-	permutationReverse(nums[index+1:])
+	permutationReverse(nums, j, n-1)
+	return
 }
 
-func permutationReverse(nums []int) {
-	l := 0
-	r := len(nums)-1
-	for l <= r {
-		nums[l], nums[r] = nums[r], nums[l]
-		l++
-		r--
+func permutationReverse(nums []int, left, right int) {
+	for left < right {
+		nums[left], nums[right] = nums[right], nums[left]
+		left++
+		right--
 	}
+	return
 }
+
+//leetcode submit region end(Prohibit modification and deletion)
